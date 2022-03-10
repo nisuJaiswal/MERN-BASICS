e111074d67fbe40dd27eb6448d6b006d9344ac97
Nisarg Jaiswal <nisujaiswal4@gmail.com>
2022-03-10 12:57:44 +0530

    Material Ui Implemented

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import Header from "../components/Header"
+
+import { Box, Container, TextField, Button, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from "react";
-import { Form, Button, Container } from 'react-bootstrap'
 import './Register.css'
 
 const Login = () => {
@@ -16,36 +18,28 @@ const Login = () => {
             [e.target.name]: e.target.value
         }));
     }
+    const theme = createTheme()
 
     const handleOnSubmit = () => { }
     return (
         <>
             <Header />
-            <Container fluid className="d-flex justify-content-center">
+            <ThemeProvider theme={theme}>
+                <Container sx={{ height: '80vh', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}>
 
-                <div className="w-50">
-                    <h3 className="mt-3">Login Here</h3>
+                    <Container sx={{ width: '40%', minWidth: '350px', padding: '4rem 1rem', border: '1px solid rgb(158,158,158)', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: '1rem', boxShadow: '7px 5px 10px grey' }}>
+                        <Typography variant="h4">Login</Typography>
+                        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '1rem' }}>
+                            <TextField onChange={handleOnChange} name="name" value={name} id="outlined-basic" size="small" sx={{ width: '150%' }} label="Username" variant="outlined" />
 
-                    <Container className="mt-4 p-5 border border-secondary rounded shadow">
+                            <TextField onChange={handleOnChange} name="pass" value={pass} id="outlined-basic" size="small" sx={{ width: '150%' }} type="email" label="Password" variant="outlined" />
 
-                        <Form onSubmit={handleOnSubmit}>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control onChange={handleOnChange} name='name' value={name} type="text" placeholder="Name" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control onChange={handleOnChange} name='pass' value={pass} type="password" placeholder="Password" />
-                            </Form.Group>
-
-                            <Button variant="primary" className="mt-2 px-3 py-2" type="submit">
-                                Log in
-                            </Button>
-                        </Form>
+                            <Button sx={{ marginTop: '1rem' }} onSubmit={handleOnSubmit} variant="contained">Register</Button>
+                        </Box>
                     </Container>
-                </div>
-            </Container>
+
+                </Container>
+            </ThemeProvider>
         </>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
+import { Box, Container, TextField, Button, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './Register.css'
-import { Form, Button, Container } from 'react-bootstrap'
 import { useState } from "react"
 const Register = () => {
     const [form, setForm] = useState({
@@ -19,49 +20,34 @@ const Register = () => {
         }));
     }
 
+    const theme = createTheme();
+
     const handleOnSubmit = () => { }
     return (
         <>
             <Header />
-            {/* <Container  className="d-flex flex-column justify-content-center"> */}
-            <div className="centerDiv">
+            <ThemeProvider theme={theme}>
+                <Container sx={{ height: '80vh', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}>
 
-                <div style={{ width: '50em' }}>
+                    <Container sx={{ width: '40%', minWidth: '350px', padding: '4rem 1rem', border: '1px solid rgb(158,158,158)', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: '1rem', boxShadow: '7px 5px 10px grey' }}>
+                        <Typography variant="h4">Registration</Typography>
+                        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '1rem' }}>
+                            <TextField onChange={handleOnChange} name="name" value={name} id="outlined-basic" size="small" sx={{ width: '150%' }} label="Username" variant="outlined" />
 
-                    <h3 className="mt-3">Enter Details to Register</h3>
-                    <Container className="mt-4 p-5 border border-secondary rounded shadow">
+                            <TextField onChange={handleOnChange} name="email" value={email} id="outlined-basic" size="small" sx={{ width: '150%' }} type="email" label="Email" variant="outlined" />
 
-                        <Form onSubmit={handleOnSubmit}>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control onChange={handleOnChange} name='name' value={name} type="text" placeholder="Name" />
-                            </Form.Group>
+                            <TextField onChange={handleOnChange} name="pass" value={pass} id="outlined-basic" size="small" type="password" sx={{ width: '150%' }} label="Password" variant="outlined" />
 
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control onChange={handleOnChange} name='email' value={email} type="email" placeholder="Email" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control onChange={handleOnChange} name='pass' value={pass} type="password" placeholder="Password" />
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control onChange={handleOnChange} name='confirmPass' value={confirmPass} type="password" placeholder="Confirm Password" />
-                            </Form.Group>
+                            <TextField onChange={handleOnChange} name="confirmPass" type="password" value={confirmPass} id="outlined-basic" size="small" sx={{ width: '150%' }} label="Confirm Password" variant="outlined" />
 
 
-
-                            <Button variant="primary" className="mt-2 px-3 py-2" type="submit">
-                                Register
-                            </Button>
-                        </Form>
+                            <Button sx={{ marginTop: '1rem' }} onSubmit={handleOnSubmit} variant="contained">Register</Button>
+                        </Box>
                     </Container>
-                </div>
-            </div>
-            {/* </Container> */}
+
+                </Container>
+            </ThemeProvider>
+
         </>
     )
 }
